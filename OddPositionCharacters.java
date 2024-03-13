84a2427448b7ee03c68faf85d334f00d14091730
Tirumala Sanjay Kumar <128822091+Sanjaykumartirumala@users.noreply.github.com>
2024-03-13 18:04:09 +0530

    Create OddPositionCharacters.java

---
+++ OddPositionCharacters.java
@@ -0,0 +1,17 @@
+public class OddPositionCharacters
+  {
+    public static void main(String[] args)
+    {
+        String text = "type here to search";
+        StringBuilder oddChars = new StringBuilder();
+        text = text.replaceAll("\\s", "");
+        for (int i = 0; i < text.length(); i++) 
+        {
+            if (i % 2 == 0)
+            {
+                oddChars.append(text.charAt(i));
+            }
+        }
+        System.out.println(oddChars.toString());
+    }
+}
